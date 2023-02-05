717e62e812de71994f65771c3d3cd1314302ef32
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-05 23:23:12 +0600

    first commit

---
+++ summation.js
@@ -0,0 +1,29 @@
+/* 
+1 to n
+1+2+3.....+n =
+
+*/
+// let num1 = 1;
+// let num2 = 2;
+// let sum = 0;
+// sum = num1 + num2;
+// console.log(sum);
+
+// n=5
+// 1+2+3+4+5+6+7+8+9+10
+
+
+// let n = 10;
+// let sum =0;
+// for(let i=1; i<10;i++){
+//    sum = sum +i;
+// }
+// console.log(sum);
+
+
+let n = 3;
+let sum =0;
+for(let i=1; i<10;i++){
+   sum = sum +i;
+}
+console.log(sum);
