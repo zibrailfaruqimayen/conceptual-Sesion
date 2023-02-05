85a20d4bd6bcb172020c3a175347020ba8c5e883
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-02-06 00:14:40 +0600

    gretar then five

---
+++ gretarThen5.js
@@ -0,0 +1,44 @@
+/* 
+input : [-1, 2, -3, 4, 5, 6, -7, 8, -9, 10]
+
+output: 3;
+*/
+
+
+// let num =4;
+// if(num < 5){
+//    console.log('YES');
+// }
+// else{
+//    console.log('NO');
+// }
+
+
+// let numbers = [-1, 2, -3, 4, 5, 6, -7, 8, -9, 10];
+// let count = 0;
+// for(let i = 0; i < numbers.length; i++){
+//    const element = numbers[i];
+//    // console.log(element);
+//    if(element > 5){
+//       // console.log('YES')
+//       count = count + 1;
+//    }
+//    else{
+//       // console.log('NO');
+//    }
+// }
+// console.log(count);
+
+function greaterThenFive(numbers) {
+   let count = 0;
+
+   for (let i = 0; i < numbers.length; i++) {
+      const element = numbers[i];
+      if (element > 5) {
+         count = count + 1;
+      }
+   }
+   return count;
+}
+const result = greaterThenFive([-1, 2, -3, 4, 5, 6, -7, 8, -9, 10])
+console.log(result);
